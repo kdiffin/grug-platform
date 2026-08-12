947aff5b98abf919f1b682f33aaa8f57ac49fb2c
kdiffin <dibrahim@azercell.com>
2026-08-12 11:03:04 +0400

    run command finished

--- main.go
+++ main.go
@@ -1,47 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// load env vars
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("couldn't load environment variables")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "usage: grug <command>")
 		os.Exit(1)
+		return
 	}
 
-	switch os.Args[1] {
+	if err := run(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+		return
+	}
+}
+
+func run(args []string) error {
+	switch args[0] {
 	case "status":
 		fmt.Println("everything is okay")
 	case "deploy":
 		fmt.Println("deploying this")
 	case "help":
 		fmt.Println("usage: grug <command>")
+	default:
+		return errors.New("this command does not exist in the grugverse: " + args[0])
 	}
 
-	fmt.Print(os.Args[0])
-	fmt.Print(os.Args[1])
-
-	log.Printf("http server started on port %v", port)
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal("failed to start http server")
-		return
-	}
+	return nil
 }
